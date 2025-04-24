4ab9a09b36d920dcd78498ba630179399846cc96
221B374 <221B374@juet.co.in>
2025-04-24 11:39:29 +0530

    LE8

---
+++ LE8/problem 1 - Adapter Pattern/IndianCrow.java
@@ -0,0 +1,5 @@
+public class IndianCrow implements Crow{
+    public void cry() {System.out.println("cry");}
+    public void fly() {System.out.println("fly");}
+    public void eat() {System.out.println("eats pearls");}
+}
---
+++ LE8/problem 1 - Adapter Pattern/Swan.java
@@ -0,0 +1,6 @@
+public interface Swan{
+    public void swim();
+    public void sing();
+    public abstract void eat();
+
+}
